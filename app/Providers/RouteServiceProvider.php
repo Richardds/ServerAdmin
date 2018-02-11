40dce4fef5e8c3af8f1a69702290b9a80e46ae47
Richard Boldiš <richard.boldis@richardds.com>
2018-02-11 13:33:06 +0100

    API Route file rename

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -27,6 +27,6 @@ class RouteServiceProvider extends ServiceProvider
         Route::prefix('api')
             ->middleware('api')
             ->namespace($this->namespace)
-            ->group(base_path('routes/ajax.php'));
+            ->group(base_path('routes/api.php'));
     }
 }
